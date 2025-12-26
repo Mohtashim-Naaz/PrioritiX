cb3d618ce425d6b345538eb956d315c69b849f88
naaz <naazcode@gmail.com>
2025-12-26 11:21:31 +0530

    test

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,7 +2,7 @@ export default function DashboardPage() {
   return (
     <div className="p-4">
       <h2 className="text-3xl font-bold underline">PrioritiX Dashboard</h2>
-      <p>Here the Eisenhower grid will come.</p>
+      <p>Here the Eisenhower okay grid will come.</p>
     </div>
   );
 }
